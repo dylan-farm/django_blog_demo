b46f1864dea1a01fa927620128b4439924393aa2
FE_Sea <mhwzhushi@163.com>
2017-10-24 16:27:23 +0800

    rss and paginator

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 # Create your models here.
@@ -7,6 +8,10 @@ class Acticle(models.Model):
     category = models.CharField(max_length=50)
     date_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField(blank=True, null=True)
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id': self.id})
+        return "http://127.0.0.1:9008%s" % path
 
     def __str__(self):
         return self.title
--- article/views.py
+++ article/views.py
@@ -1,21 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from article.models import Acticle
 from datetime import datetime
+from django.http import Http404
+from django.contrib.syndication.views import Feed  # 注意加入import语句
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger  # 添加包
 
 
 # Create your views here.
 def home(request):
-    post_list = Acticle.objects.all()  # 获取全部的Article对象
+    posts = Acticle.objects.all()  # 获取全部的Article对象
+    paginator = Paginator(posts, 2)  # 每页显示两个
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request, 'home.html', {'post_list': post_list})
 
 
-def detail(request, my_args):
-    # return HttpResponse("You're looking at my_args %s." % my_args)
-    post = Acticle.objects.all()[int(my_args)]
-    str1 = ('title=%s,category=%s,date_time=%s,content=%s' % (post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str1)
+def detail(request, id):
+    try:
+        post = Acticle.objects.get(id=str(id))
+    except Acticle.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post})
 
 
-def test(request):
-    return render(request, 'test.html', {'current_time': datetime.now()})
+def archives(request):
+    try:
+        post_list = Acticle.objects.all()
+    except Acticle.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list': post_list,
+                                             'error': False})
+
+
+def about_me(request):
+    return render(request, "aboutme.html")
+
+
+def search_tag(request, tag):
+    try:
+        post_list = Acticle.objects.filter(category=tag)  # contains
+    except Acticle.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list': post_list})
+
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request, 'home.html')
+        else:
+            post_list = Acticle.objects.filter(title__icontains=s)
+            if len(post_list) == 0:
+                return render(request, 'archives.html', {'post_list': post_list,
+                                                         'error': True})
+            else:
+                return render(request, 'archives.html', {'post_list': post_list,
+                                                         'error': False})
+    return redirect('/')
+
+
+class RSSFeed(Feed):
+    title = "RSS feed - article"
+    link = "feeds/posts/"
+    description = "RSS feed - blog posts"
+
+    def items(self):
+        return Acticle.objects.order_by('-date_time')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_pubdate(self, item):
+        return item.date_time
+
+    def item_description(self, item):
+        return item.content
